9282f367c1edc3a29bca925abeb3968bd22fa051
keymaster-hub <57302687+keymaster-hub@users.noreply.github.com>
2019-12-05 11:06:51 +0200

    Add files via upload

---
+++ rename.py
@@ -0,0 +1,97 @@
+import os
+
+
+def find_wrong_names():
+    """
+    Возвращает список *.tif файлов в текущей папке
+    не проходящих по маске А123456*.tif
+    """
+    files_list = os.listdir()
+    tif_list = []
+    wrong_list = []
+    pos = 0
+    #Создаем список *.tif файлов
+    for i in files_list:
+        if str(i)[-4:] == str('.tif'):
+            tif_list.insert(pos, i)
+            pos += 1
+    pos = 0
+    #Создаем список файлов где после 1 символа меньше 6 цифр
+    for i in tif_list:
+        if (str(i)[1:7]).isdigit() is False: 
+            wrong_list.insert(pos, i)
+            
+    return wrong_list
+
+
+def rename_files():
+    """
+    Предлагает вариант переименования
+    """
+    wrong_list = find_wrong_names()
+    wrong_list_1=[] #Список с именами на "Э" куда добавить единицы
+    wrong_list_0=[] #Список с остальными именами куда добавить нули
+    
+    for i in wrong_list:
+        if str(i[0]).lower() == str('э'):
+            num_count = 0
+            for x in str(i)[1:]:
+                if x.isdigit():
+                    num_count += 1
+                else:
+                    break
+            paste = 6 - num_count        #Сколько единиц вставить
+            paste_str = str('1') * paste #Строка из единиц
+            wrong_list_1.append(i)       #Добавляем старое имя файла
+            wrong_list_1.append(str(i[0])+str(paste_str)+str(i[1:]))#Добавляем новое имя
+        else:
+            num_count = 0
+            for x in str(i)[1:]:
+                if x.isdigit():
+                    num_count += 1
+                else:
+                    break
+            paste = 6 - num_count        #Сколько нулей вставить
+            paste_str = str('0') * paste #Строка из нулей
+            wrong_list_0.append(i)       #Добавляем старое имя файла
+            wrong_list_0.append(str(i[0])+str(paste_str)+str(i[1:]))#Добавляем новое имя
+
+    return wrong_list_1 + wrong_list_0
+
+def ask_user():
+    check = str(input('Rename? Y/N:\n')).lower().strip()
+    try:
+        if check[0] == 'y':
+            return True
+        elif check[0] == 'n':
+            return False
+        else:
+            print('Invalid Input')
+            return ask_user()
+    except Exception as error:
+        print("Please enter valid inputs")
+        print(error)
+        return ask_user()
+
+
+rename_list = rename_files()
+for i in range(0, len(rename_list), 2):
+    print(str(rename_list[i]), '-->', str(rename_list[i+1]))               
+if bool(rename_list) is True:   #Проверка не пустой ли список => все имена корректны            
+    if ask_user():
+        for i in range(0, len(rename_list), 2):
+            os.rename(rename_list[i], rename_list[i+1])
+            print(rename_list[i], ' is renamed to - ', rename_list[i+1])
+        print('Done!')
+    else:
+        print('Break')
+else:
+    print('Nothing to rename')
+
+
+    
+    
+
+
+
+
